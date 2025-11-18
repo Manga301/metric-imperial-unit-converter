638266badea2ef80e0351dd50d564e4b8e7a5522
Mangaliso <mangaliso.twala@gmail.com>
2025-11-18 14:20:19 +0200

    Update volume unit value
    Update volume unit value value from 3.78541 to 0.264172.

--- src/index.js
+++ src/index.js
@@ -12,20 +12,26 @@ let unitInput = document.getElementById("unit-input")
 let metersAndFeet = document.getElementById("meters-and-feet")
 let litersAndGallons = document.getElementById("liters-and-gallons")
 let kilogramsAndPounds = document.getElementById("kilograms-and-pounds")
+
 const lengthUnit = 3.28084
-const volumeUnit = 3.78541
+const volumeUnit = 0.264172
 const massUnit = 2.20462
+
 const convertBtn = document.getElementById("convert-btn")
 
 // convert to imperial
 const convertToImperial = (metric, unitOfMeasure) => {
+  
   const imperialValue = Number(metric * unitOfMeasure)
+
   return Number(imperialValue.toFixed(3))
 }
 
 // convert to metric
 const convertToMetric = (imperial, unitOfMeasure) => {
+  
   const metricValue = imperial / unitOfMeasure
+  
   return Number(metricValue.toFixed(3))
 }
 
@@ -40,13 +46,34 @@ convertBtn.addEventListener('click', () => {
   const unitInputValue = Number(unitInput.value)
 
   // length
-  renderConversions(metersAndFeet, unitInputValue, "meters", "feet", convertToMetric(unitInputValue, lengthUnit), convertToImperial(unitInputValue, lengthUnit))
+  renderConversions(
+                  metersAndFeet, 
+                  unitInputValue, 
+                  "meters", 
+                  "feet", 
+                  convertToMetric(unitInputValue, lengthUnit), 
+                  convertToImperial(unitInputValue, lengthUnit)
+                )
 
   // volume
-  renderConversions(litersAndGallons, unitInputValue, "liters", "gallons", convertToMetric(unitInputValue, volumeUnit), convertToImperial(unitInputValue, volumeUnit))
+  renderConversions(
+                  litersAndGallons, 
+                  unitInputValue, 
+                  "liters", 
+                  "gallons", 
+                  convertToMetric(unitInputValue, volumeUnit), 
+                  convertToImperial(unitInputValue, volumeUnit)
+                )
 
   // mass
-  renderConversions(kilogramsAndPounds, unitInputValue, "kilograms", "pounds", convertToMetric(unitInputValue, massUnit), convertToImperial(unitInputValue, massUnit))
+  renderConversions(
+                  kilogramsAndPounds, 
+                  unitInputValue, 
+                  "kilograms", 
+                  "pounds", 
+                  convertToMetric(unitInputValue, massUnit), 
+                  convertToImperial(unitInputValue, massUnit)
+                )
 
 })
 
